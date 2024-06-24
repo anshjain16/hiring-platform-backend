542eeeafc39b6e5aee4371359ce5fef19df3d276
anshjain16 <anshjain0216@gmail.com>
2024-06-25 00:47:46 +0530

    candidate controller

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./db/connect");
+const cors = require("cors");
+
 const dotenv = require("dotenv");
 const companyRoutes = require("./routes/company");
 const hiringProcessRoutes = require("./routes/hiringProcess");
@@ -7,6 +9,7 @@ const codingRouteRoutes = require("./routes/codingRound");
 const interviewRoundRoutes = require("./routes/interviewRound");
 const companyemployee = require("./routes/companyemployee");
 const question = require("./routes/programmingquestion");
+const candidateRouter = require("./routes/candidate");
 dotenv.config();
 
 const app = express();
@@ -14,6 +17,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/company", companyRoutes);
@@ -22,6 +26,7 @@ app.use("/api/codinground", codingRouteRoutes);
 app.use("/api/interviewround", interviewRoundRoutes);
 app.use("/api/companyemployee", companyemployee);
 app.use("/api/question", question);
+app.use("/api/candidate", candidateRouter);
 
 app.listen(8000, async () => {
   console.log("Server Started at port 8000");
